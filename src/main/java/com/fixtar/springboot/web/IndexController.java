5edeaf6fbfeaf1f01d540bc2e0f0157a7bfa1034
seongha <11pi885@gmail.com>
2023-05-18 03:30:10 +0900

    feat : google 로그인 구현

--- src/main/java/com/fixtar/springboot/web/IndexController.java
+++ src/main/java/com/fixtar/springboot/web/IndexController.java
@@ -1,22 +1,29 @@
 package com.fixtar.springboot.web;
 
+import com.fixtar.springboot.config.auth.dto.SessionUser;
 import com.fixtar.springboot.service.posts.PostsService;
 import com.fixtar.springboot.web.dto.PostsResponseDto;
+import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController{
 
     private final PostsService postsService;
-
-    public IndexController(PostsService postsService){this.postsService = postsService;}
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("posts",postsService.findAllDesc());
+        model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
