194f99d5adfe42cc031770a5a4b075f6c92e4ac3
seongha <11pi885@gmail.com>
2023-05-31 02:34:13 +0900

    fix : rollback

--- src/test/java/com/Fixtar/springboot/web/HelloControllerTest.java
+++ src/test/java/com/Fixtar/springboot/web/HelloControllerTest.java
@@ -1,11 +1,5 @@
 package com.Fixtar.springboot.web;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fixtar.springboot.config.auth.SecurityConfig;
 import com.fixtar.springboot.web.HelloController;
 import org.junit.jupiter.api.Test;
@@ -18,15 +12,14 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @WebMvcTest(controllers = HelloController.class,
-        excludeFilters = {
-                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
-        }
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    }
 )
 @AutoConfigureMockMvc
 public class HelloControllerTest{
@@ -39,38 +32,27 @@ public class HelloControllerTest{
     public void returnHello() throws Exception{
         String hello = "hello";
 
-//        ResultActions result =  mvc.perform(MockMvcRequestBuilders.get("/hello"));
-//
-//        result.andExpectAll(
-//                MockMvcResultMatchers.status().isOk(),
-//                MockMvcResultMatchers.content().string(hello)
-//        );
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        ResultActions result =  mvc.perform(MockMvcRequestBuilders.get("/hello"));
+
+        result.andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.content().string(hello)
+        );
     }
 
     @Test
     @WithMockUser(roles = "USER")
-    public void returnHelloDto() throws Exception{
+    public void returnHelloDto() throws Exception {
         String name = "hello";
         int amount = 10;
 
-//        ResultActions result = mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)));
-//
-//        result.andExpectAll(
-//                status().isOk(),
-//                MockMvcResultMatchers.jsonPath("$.name").value(name),
-//                MockMvcResultMatchers.jsonPath("$.amount").value(amount)
-//        );
+        ResultActions result = mvc.perform(MockMvcRequestBuilders.get("/hello/dto").param("name",name).param("amount", String.valueOf(amount)));
 
-        mvc.perform(
-                        get("/hello/dto")
-                                .param("name", name)
-                                .param("amount", String.valueOf(amount)))
-                .andExpect(status().isOk())
-                .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
-                .andExpect((ResultMatcher) jsonPath("$.amount", is(amount)));
+        result.andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.jsonPath("$.name").value(name),
+                MockMvcResultMatchers.jsonPath("$.amount").value(amount)
+        );
     }
 
 }
