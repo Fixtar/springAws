d83dfe01de400518a110a66f61765544ddfa97da
seongha <11pi885@gmail.com>
2023-06-04 21:19:30 +0900

    feat : 배포 자동화 구성하기

--- src/main/java/com/fixtar/springboot/Application.java
+++ src/main/java/com/fixtar/springboot/Application.java
@@ -2,13 +2,17 @@ package com.fixtar.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application{
 
-    public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 
 }
